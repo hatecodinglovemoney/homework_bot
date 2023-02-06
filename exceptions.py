00ed03af4a42c6f7f3baf9e87f1874a7a291ea93
Nata Butrina <hatecodinglovemoney@gmail.com>
2023-02-06 22:23:29 +0600

    Commit 4

--- exceptions.py
+++ exceptions.py
@@ -1,18 +1,3 @@
-class WrongGetApiAnswer(Exception):
-    """Неверный статус ответа API"""
-    pass
-
-
 class TelegramError(Exception):
     """Ошибка отправки сообщения в Telegram"""
-    pass
-
-
-class WrongCheckResponse(Exception):
-    """Неверное содержание ответа API"""
-    pass
-
-
-class WrongParseStatus(Exception):
-    """Неверное содержание ответа API"""
     pass
